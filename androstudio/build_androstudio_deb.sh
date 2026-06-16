cbe2705975b678091cb524e7a4d1159d758471c2
jahangir <btblmedia.924@gmail.com>
2026-06-17 02:54:40 +0600

    update nevironment setup pkg androstudio

--- androstudio/build_androstudio_deb.sh
+++ androstudio/build_androstudio_deb.sh
@@ -11,10 +11,10 @@
 set -e
 
 PKG_NAME="androstudio"
-PKG_VERSION="2.6"
+PKG_VERSION="2.7"
 PKG_ARCH="all"
 PKG_MAINTAINER="AndroStudio <repo@androstudio.dev>"
-PKG_DEPENDS="git, which, curl, dpkg, aapt2"
+PKG_DEPENDS="git, which, curl, dpkg, aapt2, unzip, tar, findutils, sed, grep, gawk"
 PKG_DESCRIPTION="AndroStudio IDE environment setup
  Installs and configures all required dependencies for
  AndroStudio IDE on Android. Use 'androstudio setup' to
